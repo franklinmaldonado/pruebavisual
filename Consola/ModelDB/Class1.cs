e93122b69ee28be896c3f3e4b280fb4f6b4f8e65
Franklin Maldonado <60884549+franklinmaldonado@users.noreply.github.com>
2022-02-05 23:15:52 -0500

    2

--- Consola/ModelDB/Class1.cs
+++ Consola/ModelDB/Class1.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 
+using Microsoft.EntityFrameworkCore;
+using Modelo.Entidades;
+
 namespace ModelDB
 {
-    public class Class1
+    public class Class1 : DbContext
     {
+        public DbSet<film> films { get; set; }
+        public DbSet<inventory> inventorys { get; set; }
+        public DbSet<language> languages { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder opciones)
+        {
+            opciones.UseSqlServer("Server= DESKTOP-84TGTM8 ; initial catalog = dvd; trusted_connection = true; ");
+
+            
+        }
     }
 }
